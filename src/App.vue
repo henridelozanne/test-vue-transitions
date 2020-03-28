683321ec12402245cd30a395bbd0e97ef2b1bd7a
Henri Delozanne <hnr.delozanne@gmail.com>
2020-03-28 11:56:32 +0100

    Create vue-transition on modal enter and leave.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class="button" @click="toggleModal">Launch modal</button>
+    <transition name="fade">
+      <div v-if="modalVisible" class="modal">
+        <div class="modal-content">
+          Modal component
+          <button class="button quit-button" @click="toggleModal">Quit modal</button>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      modalVisible: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.modalVisible = !this.modalVisible;
+    },
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +36,56 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 1.3em;
+}
+
+.button {
+  padding: 5px 10px;
+  background: teal;
+  color: white;
+  border-radius: 5px;
+  font-size: 18px;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.quit-button {
+  margin-top: 50px;
+}
+
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+
+  .modal-content {
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+    max-width: 500px;
+    margin: 200px auto;
+    padding: 100px 50px;
+    border-radius: 5px;
+  }
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
